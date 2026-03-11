9c9a8d8eba21e01f8208f37952f2550dcca80af6
Roescoe <52107557+roescoe@users.noreply.github.com.>
2026-03-10 20:11:44 -0700

    Update version

--- elite_colonisationv2.py
+++ elite_colonisationv2.py
@@ -76,7 +76,7 @@ class UI(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(UI, self).__init__()
         #properties
-        self.subVersion =".15a"
+        self.subVersion =".16a"
         self.olderThanNumDays = 0
         self.allTextSize = 12
         self.logfiles = []
